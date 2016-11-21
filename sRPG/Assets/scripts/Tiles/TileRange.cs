1b10c045033a13fb4240fc61b59f5102945eb2f2
Sunjiawei58 <sunjiawei5548833@163.com>
2016-11-20 20:07:19 -0500

    update 11.20
    add NewTIile.cs Tile.cs TileRange.cs

---
+++ sRPG/Assets/scripts/Tiles/TileRange.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TileRange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public static List<Tile> FindHighlight(Tile originTile, int movementPoints, Vector2[] occupied, bool staticRange)
+    {
+        List<Tile> closed = new List<Tile>();
+        List<Tile> open = new List<Tile>();
+        List<Tile> result = new List<Tile>();
+        //int i = Tile.x - movementPoints;
+        float leftMax = Mathf.Max(originTile.gridPosition.x - movementPoints, 0) ;
+        float rightMax = Mathf.Min(originTile.gridPosition.x + movementPoints, GameManager.instance.mapSize -1);
+        float upMax = Mathf.Max(originTile.gridPosition.y - movementPoints, 0);
+        float downMax = Mathf.Min(originTile.gridPosition.y + movementPoints, GameManager.instance.mapSize - 1);
+        for (int i=(int)leftMax;i<(int)rightMax;i++)
+        {
+            for(int j = (int)upMax; j < (int)downMax; j++)
+            {
+                Tile tmpTile = new Tile();
+                tmpTile.gridPosition.x = i;
+                tmpTile.gridPosition.y = j;
+
+                List<Tile> path = new List<Tile>();
+               // tmpTile.path = AStar(originTile,tmpTile) // a star return a path.
+              // tmpTile.cost = ???
+              if(tmpTile.cost <= movementPoints)
+                {
+                    result.Add(tmpTile);
+                }
+            }
+        }
+
+        return result;
+    }
+}
