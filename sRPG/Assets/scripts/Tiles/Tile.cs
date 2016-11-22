fa1455bcf00908b6c782f54f23ff6006139653db
tsq910621 <tsq910621@gmail.com>
2016-11-21 20:40:16 -0500

    11.21
    tile.cs
    tilerange.cs

--- sRPG/Assets/scripts/Tiles/Tile.cs
+++ sRPG/Assets/scripts/Tiles/Tile.cs
@@ -16,9 +16,21 @@ public class Tile : MonoBehaviour {
 	public bool impassible = false;
 	
 	public List<Tile> neighbors = new List<Tile>();
+	public Tile parent;
 
     public List<Tile> path = new List<Tile>();
+
     public int cost = 0 ;     //  999999?
+
+	public int gCost;
+	public int hCost;
+	public int fCost {
+		get {
+			return gCost + hCost;
+		}
+	}
+
+
 	// Use this for initialization
 	void Start () {
 		if (Application.loadedLevelName == "gameScene") generateNeighbors();
